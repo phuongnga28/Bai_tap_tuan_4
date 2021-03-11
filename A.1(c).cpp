0617eae7399505e758b1ae299e874c9a60ff6022
Nguyen Phuong Nga <nguyenphuongnga289@gmail.com>
2021-03-11 22:01:01 +0700

    first commit

---
+++ A.1(c).cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+	int a[] = {};
+	int temp;
+	int n = sizeof(a) / sizeof(a[0]);
+	cout << "length = " << n << endl;
+	for(int i=0; i<n;i++){
+		for(int j=i+1;j<n+1;j++){
+			if(a[i] > a[j]){
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	for(int i=1; i<=n;i++){
+		cout << a[i] << " ";
+	}
+	return 0;
+}
