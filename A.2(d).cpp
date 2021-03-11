89ca64c1c93631c593f66c26d6ea4ca2ee02a74c
Phương Nga Nguyễn <78184262+phuongnga28@users.noreply.github.com>
2021-03-11 22:20:18 +0700

    Add files via upload

---
+++ A.2(d).cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+	int a[] = {7};
+	int n=4;
+	bool b=false;
+	for(int i=0;i<sizeof(a) / sizeof(a[0]);i++){
+		if(a[i] == n){
+		b=true;
+		break;
+		}
+	}
+	if(b==true) {cout << "Found";}
+	else {cout << "Not Found";}
+	return 0;
+}
---
+++ A.2(g).cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+	int a[] = {7,5,3,1,0};
+	int n=0;
+	bool b=false;
+	for(int i=0;i<sizeof(a) / sizeof(a[0]);i++){
+		if(a[i] == n){
+		b=true;
+		break;
+		}
+	}
+	if(b==true) {cout << "Found";}
+	else {cout << "Not Found";}
+	return 0;
+}
